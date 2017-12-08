d61ed3f05edbee2085a56cb11ebc472055b8cd92
Guillaume Lebeau <guillaume.lebeau@irec.fr>
2017-12-08 10:52:07 +0100

    Step 13: Selected state IDs

--- src/products/store/selectors/toppings.selectors.ts
+++ src/products/store/selectors/toppings.selectors.ts
@@ -13,6 +13,11 @@ export const getToppingsState = createSelector(
 
 export const getToppingEntities = createSelector(getToppingsState, fromToppings.getToppingEntities);
 
+export const getSelectedToppings = createSelector(
+  getToppingsState,
+  fromToppings.getSelectedToppings,
+);
+
 export const getAllToppings = createSelector(getToppingEntities, entities => {
   return Object.keys(entities).map(id => entities[parseInt(id, 10)]);
 });
